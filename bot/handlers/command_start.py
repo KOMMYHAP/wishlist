7db7c6c1fcb8ed376a535739d4444f02ded4bb87
ppoliakov <pasha.u.polyak@gmail.com>
2023-11-23 23:44:20 +0300

    - edit start message for new users

--- bot/handlers/command_start.py
+++ bot/handlers/command_start.py
@@ -18,5 +18,19 @@ async def command_start_handler(message: Message, bot: AsyncTeleBot, wish_manage
 
     new_user_created = await wish_manager.register_user(user)
     if new_user_created:
-        await bot.send_message(message.chat.id, f'Привет, {user.name}!')
+        hello_world_message = f"""Привет, {user.name}!
+
+Если тебя пригласили на день рождения и ты хочешь подарить именинику что-то действительно нужное, но не знаешь что, то я постараюсь тебе помочь!
+
+Я могу показать тебе список желаний человека, а ты в свою очередь можешь рассказать мне свой, чтобы помочь другим людям выбрать и тебе качественный подарок.
+
+ВНИМАНИЕ. 
+Я НАХОЖУСЬ В АКТИВНОЙ РАЗРАБОТКЕ. 
+БАГИ, ЗАВИСАНИЯ, НЕУДОБСТВА, ПОТЕРЯ ДАННЫХ - ВСЁ ЭТО МОИ ЛУЧШИЕ ДРУЗЬЯ.
+
+Большое вам спасибо, что помогаете делать меня лучше.
+Со всем пожеланиями, идеями, баг-репортами вы можете обращаться непосредственно к моему разработчику: @polyakov_white   
+"""
+        await bot.send_message(message.chat.id, hello_world_message)
+
     await show_wishlist(message.from_user, bot, wish_manager, 0)
