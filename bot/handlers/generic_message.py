715c61bf2ca07785f0b09b54dbb186cac8e2eb1a
ppoliakov <pasha.u.polyak@gmail.com>
2023-12-08 03:17:14 +0300

    - fix ping/pong

--- bot/handlers/generic_message.py
+++ bot/handlers/generic_message.py
@@ -8,6 +8,7 @@ async def generic_message_handler(message: Message, bot: AsyncTeleBot, logger: L
     logger = logger.getChild('wish_generic_message')
     if message.text == "ping":
         await bot.reply_to(message, "pong")
+        return
 
     logger.warning("Unknown message? User talks with bot? User's text: '%s'", message.text)
     await bot.reply_to(message, "Прости, кажется я тебя не понял.")
