9ebe0cbff737fe303e2ba00cb5a69b38812599f0
ppoliakov <pasha.u.polyak@gmail.com>
2024-03-31 01:24:32 +0300

    check database return value in migration tool

--- wish/migration/migration_to_postgresql.py
+++ wish/migration/migration_to_postgresql.py
@@ -11,17 +11,24 @@ async def perform_migration(logger: Logger, memory_storage: WishStorageMemoryAda
         if not user_data:
             logger.error('Invalid user stored by id %d!', user_id)
             return False
-        await database_storage.create_user(user_data)
+        if not await database_storage.create_user(user_data):
+            logger.error('Failed to create user %d!', user_id)
+            return False
 
+    for user_id in memory_storage.users.keys():
         friend_list = await memory_storage.get_friend_list(user_id)
         for friend_record in friend_list:
-            await database_storage.create_friend(user_id, friend_record)
+            if not await database_storage.create_friend(user_id, friend_record):
+                logger.error('Failed to create friend %d for user %d!', friend_record.user.id, user_id)
+                return False
 
     for wish_id in memory_storage.wishes.keys():
         wish_data = await memory_storage.get_wish(wish_id)
         if not wish_data:
             logger.error('Invalid wish stored by id %d!', wish_id)
             return False
-        await database_storage.create_wish(wish_data)
+        if not await database_storage.create_wish(wish_data):
+            logger.error('Failed to create wish %d!', wish_id)
+            return False
 
     return True
