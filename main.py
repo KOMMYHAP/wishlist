2362dd890674d333c075844c5980c95fa2f637a5
ppoliakov <pasha.u.polyak@gmail.com>
2024-03-31 01:22:54 +0300

    remove main.py, you should specify explicit what entry point you want to run

--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from asyncio import WindowsSelectorEventLoopPolicy
-
-from bot.telegram_bot import entry_point
-
-if __name__ == '__main__':
-    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
-    asyncio.run(entry_point())
