e7aa5e7eb818cb298dab6ddf6921ea0290d4ff1a
ppoliakov <pasha.u.polyak@gmail.com>
2024-06-13 00:53:10 +0300

    any stderr from subprocess interprets as error

--- wish/storage_adapters/postgresql/scripts/backup_utils.py
+++ wish/storage_adapters/postgresql/scripts/backup_utils.py
@@ -15,10 +15,14 @@ def collect_backups_list(root: str) -> list[str]:
 
 def run_external_tool(args) -> bool:
     try:
-        subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True, check=True)
-        return True
+        result = subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True, check=True)
+        if result.stderr:
+            print(result.stderr)
+            return False
     except subprocess.CalledProcessError as e:
         print(e)
         if e.stderr:
             print(e.stderr)
         return False
+
+    return True
