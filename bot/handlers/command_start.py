8a7482f5fb4ee0349f2af6164bff5631f6b8f6af
ppoliakov <pasha.u.polyak@gmail.com>
2024-11-17 19:00:31 +0300

    parse user id from /start command to view its wishlist

--- bot/handlers/command_start.py
+++ bot/handlers/command_start.py
@@ -6,6 +6,7 @@ from telebot.types import Message
 
 from bot.handlers.command_registry import WishlistCommands
 from bot.handlers.wish_editor.wishlist_editor import send_my_wishlist_editor
+from bot.handlers.wish_viewer.wishlist_viewer import send_user_wishlist_viewer
 from bot.utilities.user_fullname import get_user_fullname
 from bot.version import current_bot_version
 from wish.types.user import User
@@ -26,7 +27,38 @@ async def command_start_handler(message: Message, bot: AsyncTeleBot, wish_manage
         hello_world_message = f"Привет, {get_user_fullname(user)}!\n\n{_start_message}"
         await bot.send_message(message.chat.id, hello_world_message)
 
-    await send_my_wishlist_editor(logger, message, bot, wish_manager, 0)
+    if len(message.text) > 0:
+        await _command_start_with_deeplink(message, bot, wish_manager, logger)
+    else:
+        await send_my_wishlist_editor(logger, message, bot, wish_manager, 0)
+
+
+async def _command_start_with_deeplink(message: Message, bot: AsyncTeleBot, wish_manager: WishManager,
+                                       logger: Logger) -> None:
+    target_user_id = _try_get_deep_linked_user_id(message.text)
+    if target_user_id is None:
+        await bot.reply_to(message, 'Некорректная ссылка! Попробуй попросить новую ссылку у твоего друга')
+        return
+
+    requested_user = await wish_manager.find_user_by_id(target_user_id)
+    if not requested_user:
+        await bot.reply_to(message,
+                           'Я не смог найти вишлист этого человека! Попробуй попросить новую ссылку у твоего друга')
+        return
+
+    await send_user_wishlist_viewer(bot, logger, message, target_user_id, wish_manager, 0)
+
+
+def _try_get_deep_linked_user_id(text: str) -> None | int:
+    prefix = '/start '
+    if not text.startswith(prefix):
+        return None
+
+    parameter = text.removeprefix(prefix)
+    try:
+        return int(parameter)
+    except ValueError:
+        return None
 
 
 _start_message = f"""Если ты хочешь получить в подарок что-то действительно нужное...
@@ -39,6 +71,5 @@ _start_message = f"""Если ты хочешь получить в подаро
 Ты можешь создать свой список желаний /{WishlistCommands.MY_WISHLIST.value}, чтобы помочь другим людям выбрать и тебе качественный подарок.
 Также ты можешь следить за обновлением списка желаний своих друзей /{WishlistCommands.GET_UPDATES.value}.
 
-Большое вам спасибо, что помогаете делать меня лучше.
-Со всем пожеланиями, идеями, баг-репортами вы можете обращаться непосредственно к моему разработчику: @polyakov_white.
+Большое спасибо, что помогаешь делать меня лучше!
 """
