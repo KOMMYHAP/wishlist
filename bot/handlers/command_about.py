c177c42cdc8964f70f59c6456091a57414b19631
ppoliakov <pasha.u.polyak@gmail.com>
2024-11-17 20:39:30 +0300

    update message in /about

--- bot/handlers/command_about.py
+++ bot/handlers/command_about.py
@@ -3,30 +3,36 @@ from logging import Logger
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
+from bot.handlers.command_registry import WishlistCommands
 from wish.types.user import User
 from wish.wish_manager import WishManager
 
 
-async def command_about_handler(message: Message, bot: AsyncTeleBot, logger: Logger) -> None:
+async def command_about_handler(message: Message, bot: AsyncTeleBot, logger: Logger, wish_manager: WishManager) -> None:
     logger = logger.getChild('about_command')
     message_sender = message.from_user
 
-    username = message_sender.username or ''
     user_id = message_sender.id
-    logger.info('User "%s" (id: %s) wants to know more about me!', username, user_id)
+    user = await wish_manager.find_user_by_id(user_id)
+    if user is None:
+        logger.error('User "%d" was not registered!', user_id)
+        return
 
-    await bot.send_message(message.chat.id, _about_message)
+    await bot.send_message(message.chat.id, _generate_about_message(user, wish_manager))
 
 
 def _generate_share_link(user: User, wish_manager: WishManager) -> str:
     return f'https://t.me/{wish_manager.config.bot_username}?start={user.id}'
 
 
-_about_message = f"""Данный бот изначально являлся домашним проектом для автора и его узкого круга друзей, однако перерос в нечто большее.
-И сейчас это нечто требует время разработчика на поддержку сервера, исправление проблем и добавление нового функционала :) 
+def _generate_about_message(user: User, wish_manager: WishManager) -> str:
+    return f"""О боте:
+/{WishlistCommands.MY_WISHLIST.value} — показать свой список желаний
+/{WishlistCommands.USER_WISHLIST.value} — показать список желаний других пользователей
+/{WishlistCommands.GET_UPDATES.value} — показать обновления желаний у других пользователей 
+/{WishlistCommands.ABOUT.value} — показать данное сообщение
 
-Ваши пожелания, идеи и баг-репорты приветствуются здесь: @polyakov_white.
-А благодарности здесь: +79994638280, https://ko-fi.com/polyakov_white.
-
-Спасибо!
+{_generate_share_link(user, wish_manager)} — поделиться своим списком желаний  
+https://ko-fi.com/polyakov_white — купить автору кофе
+https://t.me/polyakov_white — выразить благодарности автору
 """
